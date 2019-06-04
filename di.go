b813159b8536c07701fa26f9e0614fd598bc040f
Ben Ramsay <spacedmonkeytct@gmail.com>
2019-06-04 07:52:48 +0100

    Generalise Greet to use interface io.Writer

--- di.go
+++ di.go
@@ -1,11 +1,16 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"os"
 )
 
 // Greet ...
-func Greet(writer *bytes.Buffer, name string) {
+func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func main() {
+	Greet(os.Stdout, "Elodie")
 }
